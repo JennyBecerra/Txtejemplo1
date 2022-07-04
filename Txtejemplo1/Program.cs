e187c1702fcb9796f6e67a3a07a035879aac7a56
jenny <jennybecerra85@gmail.com>
2022-07-04 11:29:04 -0500

    agregando un ejemplo de lectura de txt en c#

---
+++ Txtejemplo1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Txtejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            try
+            {
+                StreamReader sr = new StreamReader(@"C:\Users\jenni\source\repos\sample.txt");
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: "+e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
